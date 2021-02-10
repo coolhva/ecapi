7382ec1e56c7bec4cd62e561165bb23698d0b948
Henk van Achterberg <coolhva@gmail.com>
2021-02-10 15:36:49 +0100

    type

--- app/auth/email.py
+++ app/auth/email.py
@@ -4,7 +4,7 @@ from app.email import send_email
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email('[Email.Cloud IOC API Management] Reset Your Password',
-               sender=current_app.config['MAILFROM'],
+               sender=current_app.config['MAIL_FROM'],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
                                          user=user, token=token),
