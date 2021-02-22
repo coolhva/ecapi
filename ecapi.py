e0bf3c645c95d5d87b37825b7cae0e31dd8410fe
Henk van Achterberg <coolhva@gmail.com>
2021-02-22 21:29:16 +0100

    update license

--- ecapi.py
+++ ecapi.py
@@ -1,3 +1,11 @@
+"""Email.Cloud IOC Management
+
+This application allows you to manage the Indicator of Compromise feature of
+Email.Cloud through a webapplication. Register a user, enter API credentials,
+log in and you are ready to start the app.
+
+Author: henk.vanachterberg@broadcom.com
+"""
 from app import create_app, db
 from app.models import User
 
